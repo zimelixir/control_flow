7c48d2dd5814f90f4c15f6458a6d4f6d755a92e3
Blessed <zimelixir@gmail.com>
2024-10-04 08:30:02 +0200

    Create mix pproject

---
+++ lib/control_flow.ex
@@ -0,0 +1,18 @@
+defmodule ControlFlow do
+  @moduledoc """
+  Documentation for `ControlFlow`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ControlFlow.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
